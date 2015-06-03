999d14e85b8eed5d8ce00c31b4bb890cc9bb0dee
snandhm <2528682029@qq.com>
2015-06-03 19:01:21 +0800

    Update my_stack.c

--- final-test/c_cpp/my_stack.c
+++ final-test/c_cpp/my_stack.c
@@ -4,5 +4,59 @@
  * 但是记住g++才是编译C++的编译器
  *
  */
+#include "stack.h"
+#include <assert.h>
+#include <stdlib.h>
+ 
+void stack_init(struct stack *st, int capability)
+{
+    assert(st && capability > 0);
+    st->data = (ElemType *)malloc(sizeof(ElemType) * capability);
+    assert(st->data);
+    st->top = st->data;
+    st->capability = capability;
+}
+ 
+ 
+int stack_push(struct stack *st, ElemType elem)
+{
+    assert(st);
+    if (stack_full(st))
+        return STACK_OVERFLOW;
+    *(st->top++) = elem;
+    return STACK_OK;
+}
+ 
+int stack_pop(struct stack *st)
+{
+    assert(st);
+    if (stack_empty(st)) 
+        return STACK_OVERFLOW;
+    st->top--;
+    return STACK_OK;
+}
+ 
 
+int stack_size(const struct stack *st)
+{
+    assert(st);
+    return (st->top - st->data);
+}
+ 
+int stack_capability(const struct stack *st)
+{
+    assert(st);
+    return st->capability;
+}
+ 
+int stack_full(const struct stack *st)
+{
+    return (stack_size(st) == stack_capability(st));
+}
+ 
+int stack_empty(const struct stack *st)
+{
+    return (stack_size(st) == 0);
+}
+ 
 
